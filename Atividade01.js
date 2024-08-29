971b0d212307760bf7bedfc2c11b1a4f95a0e0ab
EduardoVitor020 <160738317+EduardoVitor020@users.noreply.github.com>
2024-08-29 20:18:50 -0300

    Add files via upload

---
+++ Atividade01.js
@@ -0,0 +1,7 @@
+// let é variavel, então declarei a variavel valor1 e valor2.
+let valor1 = 46;
+let valor2 = 75;
+// declarei a variavel resultado, e somando já.
+let resultado = valor1 + valor2;
+// console.log é o que vai imprimir aquilo que eu colocar dentro dos parenteses.
+console.log(`O resultado é: ${resultado}`);
---
+++ Atividade01.php
@@ -0,0 +1,6 @@
+<?php
+// <?php é o que permite a abertura do arquivo PHP
+// echo é o comando que vai enviar meu texto para tela ou terminal
+echo "Hello World!!";
+echo "Esse é meu primeiro código em PHP.";
+?>
